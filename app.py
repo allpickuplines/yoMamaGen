948897b62c947e492564097dfb1a44c9901757d4
kreamt <karimtabet@gmail.com>
2013-07-12 14:03:05 +0100

    setup app with sqlite db and joke model

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_peewee.db import Database
+from peewee import *
+
+# configure our database
+DATABASE = {
+    'name': 'yomama.db',
+    'engine': 'peewee.SqliteDatabase',
+}
+DEBUG = True
+SECRET_KEY = 'ssshhhh'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# instantiate the db wrapper
+db = Database(app)
+
+
+class Joke(db.Model):
+    joke = TextField()
+    categories = TextField()
+
+
+if __name__ == '__main__':
+    Joke.create_table(fail_silently=True)
+
+    app.run()
