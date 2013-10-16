f91b13fd59ad19aeeaf2c75df161a7e1e0ffe1dc
kreamt <karimtabet@gmail.com>
2013-10-16 19:06:45 +0100

    refactors striptags

--- yoMamaSpider/striptags.py
+++ yoMamaSpider/striptags.py
@@ -1,5 +1,17 @@
 import re
 
-def strip_tags(title):
-    return re.sub(r'(?i)(yo|mama|is|so|her|are|joke|jokes|
-        and|[^\w]|^\s+)', '', title)
+stopwords = []
+
+for line in open('yoMamaSpider/stopwords.txt'):
+    stopwords.append(line.strip('\n'))
+
+
+def striptags(tag):
+    tag = re.sub(r'(?i)([^\w]|^\s+|[0-9])', '\n', str(tag))
+    remainder = []
+    words = tag.split('\n')
+    for word in words:
+        if word != '':
+            if word.lower() not in stopwords:
+                remainder.append(word)
+    return ','.join(remainder)
