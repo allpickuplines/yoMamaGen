9b8b0d487ec45d1787a9ca725c91e1fc4855c980
kreamt <karimtabet@gmail.com>
2013-12-17 00:29:26 +0000

    fixes to  categories in models

--- app/models.py
+++ app/models.py
@@ -24,13 +24,37 @@ if __name__ == '__main__':
         Jokes.categories).all()
 
     for category in categories:
-        category = re.sub(r'(?i)([^\w])', '', str(category)[2:])
+        category = re.sub(r'(?i)([^\w\s])', '', str(category)[2:])
         print category
-        jokes = db.session.query(Jokes).filter(
-            Jokes.joke.contains(
-                ' ' + category.lower() + ' ')).all()
-
-        for joke in jokes:
-            if category not in joke.categories:
-                joke.categories += ',' + category
+        if (category == 'Star Trek Wars Science Fiction Nerds'
+            or category == 'Nerd Geek'
+            or category == 'Math Related'):
+            new_category = ['Geeky']
+        elif category == 'Such Slut':
+            new_category = ['Slut']
+        elif category == 'Dumb Stupid':
+            new_category = ['Stupid']
+        elif category == 'Barack Obama':
+            new_category = ['Political']
+        elif category == 'Dirty Nasty Greasy':
+            new_category = ['Dirty', 'Nasty', 'Greasy']
+        else:
+            new_category = [category]
+        
+        for cat in new_category:
+            #Change name of initial category to new category
+            jokes = db.session.query(Jokes).filter(
+                Jokes.categories.contains(category)).all()
+            for joke in jokes:
+                joke.categories = cat
                 db.session.commit()
+            
+            #Add categories to relevant jokes
+            jokes = db.session.query(Jokes).filter(
+                Jokes.joke.contains(
+                    ' ' + cat.lower() + ' ')).all()
+
+            for joke in jokes:
+                if cat not in joke.categories:
+                    joke.categories += ',' + cat
+                    db.session.commit()
